14352a53c2663d07cee3387bbd9c8b44b474ffd9
basirat10 <basiratamusat10@gmail.com>
2024-02-17 15:57:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// VARIABLE:Global and local scope variables
+
+// Global Variable
+
+let i;
+for(i = 0; i < 5; i++){
+    let p = 10;
+    console.log(i+p)
+}
+console.log(i)
+
+// OPERATORS AND EQUALITY
+// Arithmetical operators
+
+// +,-,*,/,%,==.===, increment
+
+// minus (-);
+
+let costs = 15
+let profits;
+let salesprices = 20
+
+profits = salesprices - costs;
+
+console.log(profits)
+
+
+
+// plus (+)
+
+let cost = 15, profit = 5, salesprice = cost + profit;
+console.log(salesprice);
+
+
+let firsNames = "Olatunde", surnames = "Tobi ", fullName = surnames + firsNames;
+console.log(fullName + " " + 'is a big boy')
+
+
+let firsName = "Olatunde", surname = "Tobi ", fullNames = surname + firsName;
+console.log(`${fullNames} is a big boy`)
+
+
+
+// Back tick (``)
+let food = 'Rice'
+console.log(`what will i eat, if not ${food} ?`);
+
+let A = 2;
+A *= 4;
+console.log(A)
